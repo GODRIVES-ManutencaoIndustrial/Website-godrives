c7a548ff567a3c537234029cd5ad473a9d4abef3
Gabriel Manicucci <gabrielmanicucci@outlook.com>
2024-07-25 14:30:30 -0300

    feat: faature close mobile menu is ready

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,32 +2,33 @@
 import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuLabel,
+//   DropdownMenuSeparator,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu"
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+// import {
+//   NavigationMenu,
+//   NavigationMenuContent,
+//   NavigationMenuIndicator,
+//   NavigationMenuItem,
+//   NavigationMenuLink,
+//   NavigationMenuList,
+//   NavigationMenuTrigger,
+//   NavigationMenuViewport,
+// } from "@/components/ui/navigation-menu"
 
 import { TbMenuDeep } from "react-icons/tb"
 import { IoMenuOutline } from "react-icons/io5"
@@ -35,6 +36,7 @@ import { FiMenu } from "react-icons/fi"
 import { SlMenu } from "react-icons/sl"
 import { HiOutlineMenuAlt3 } from "react-icons/hi"
 import { Divide } from "lucide-react"
+import { usePathname } from "next/navigation"
 
 const getInitialSreenX = () => {
   if (typeof window !== "undefined") {
@@ -46,6 +48,7 @@ const getInitialSreenX = () => {
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false)
   const [screenX, setScreenX] = useState<number>(getInitialSreenX())
+  const pathname = usePathname()
 
   useEffect(() => {
     const handleScroll = () => {
@@ -106,44 +109,54 @@ export default function Navbar() {
             </SheetTrigger>
             <SheetContent className="flex flex-col items-center gap-10 bg-primaryBlue p-0 pt-10">
               <SheetHeader className="m-0 mt-10 w-full space-y-0">
-                <Link
-                  href={"/"}
-                  className="h-full w-full py-5 text-center transition-all hover:bg-blue600"
-                >
-                  Início
-                </Link>
-                <Link
-                  href={"/routes/empresa"}
-                  className="h-full w-full py-5 text-center transition-all hover:bg-blue600"
-                >
-                  Empresa
-                </Link>
-                <Link
-                  href={"/routes/servicos"}
-                  className="h-full w-full py-5 text-center transition-all hover:bg-blue600"
-                >
-                  Serviços
-                </Link>
+                <SheetClose asChild>
+                  <Link
+                    href={"/"}
+                    className={`h-full w-full py-5 text-center transition-all hover:bg-blue600 ${pathname === "/" ? "bg-blue400" : ""}`}
+                  >
+                    Início
+                  </Link>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Link
+                    href={"/routes/empresa"}
+                    className={`h-full w-full py-5 text-center transition-all hover:bg-blue600 ${pathname === "/routes/empresa" ? "bg-blue400" : ""}`}
+                  >
+                    Empresa
+                  </Link>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Link
+                    href={"/routes/servicos"}
+                    className={`h-full w-full py-5 text-center transition-all hover:bg-blue600 ${pathname === "/routes/servicos" ? "bg-blue400" : ""}`}
+                  >
+                    Serviços
+                  </Link>
+                </SheetClose>
               </SheetHeader>
 
-              <button
-                onClick={() =>
-                  (window.location.href = "https://wa.me/5562998817695")
-                }
-                className="inline-flex h-[56px] w-64 items-center justify-center gap-5 rounded-[28px] border-2 border-secondary900 bg-primaryBlue fill-secondary900 py-3 text-blue900 transition-all hover:bg-secondary900 hover:fill-primaryBlue hover:text-primaryBlue"
-              >
-                <p className="font-['Poppins'] text-xl font-normal ">
-                  Contate-nos
-                </p>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="27"
-                  height="16"
-                  viewBox="0 0 27 16"
+              <SheetClose asChild>
+                <button
+                  onClick={() =>
+                    (window.location.href = "https://wa.me/5562998817695")
+                  }
+                  className="inline-flex h-[56px] w-64 items-center justify-center gap-5 rounded-[28px] border-2 border-secondary900 bg-primaryBlue fill-secondary900 py-3 text-blue900 transition-all hover:bg-secondary900 hover:fill-primaryBlue hover:text-primaryBlue"
                 >
-                  <path d="M26.7071 8.70711C27.0976 8.31658 27.0976 7.68342 26.7071 7.29289L20.3431 0.928932C19.9526 0.538408 19.3195 0.538408 18.9289 0.928932C18.5384 1.31946 18.5384 1.95262 18.9289 2.34315L24.5858 8L18.9289 13.6569C18.5384 14.0474 18.5384 14.6805 18.9289 15.0711C19.3195 15.4616 19.9526 15.4616 20.3431 15.0711L26.7071 8.70711ZM0 9H26V7H0V9Z" />
-                </svg>
-              </button>
+                  <p className="font-['Poppins'] text-xl font-normal ">
+                    Contate-nos
+                  </p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="27"
+                    height="16"
+                    viewBox="0 0 27 16"
+                  >
+                    <path d="M26.7071 8.70711C27.0976 8.31658 27.0976 7.68342 26.7071 7.29289L20.3431 0.928932C19.9526 0.538408 19.3195 0.538408 18.9289 0.928932C18.5384 1.31946 18.5384 1.95262 18.9289 2.34315L24.5858 8L18.9289 13.6569C18.5384 14.0474 18.5384 14.6805 18.9289 15.0711C19.3195 15.4616 19.9526 15.4616 20.3431 15.0711L26.7071 8.70711ZM0 9H26V7H0V9Z" />
+                  </svg>
+                </button>
+              </SheetClose>
             </SheetContent>
           </Sheet>
         )}
@@ -151,13 +164,19 @@ export default function Navbar() {
         {screenX > 768 && (
           <>
             <ul className="flex items-center gap-8 text-primaryBlue">
-              <li className="transition-all hover:brightness-150">
+              <li
+                className={`text-primaryBlue transition-all hover:brightness-150 ${pathname === "/" ? "brightness-150" : ""}`}
+              >
                 <a href="/">Início</a>
               </li>
-              <li className="transition-all hover:brightness-150">
+              <li
+                className={`text-primaryBlue transition-all hover:brightness-150 ${pathname === "/routes/empresa" ? "brightness-150" : ""}`}
+              >
                 <a href="/routes/empresa">Empresa</a>
               </li>
-              <li className="transition-all hover:brightness-150">
+              <li
+                className={`text-primaryBlue transition-all hover:brightness-150 ${pathname === "/routes/servicos" ? "brightness-150" : ""}`}
+              >
                 <a href="/routes/servicos">Serviços</a>
               </li>
             </ul>
